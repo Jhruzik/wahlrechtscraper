f15b3116cbf8e2cecbb2a11b577e83669c89fb88
Jhruzik <joshua.hruzik@gmail.com>
2019-11-23 12:50:57 +0100

    Added Main Method to commit

--- src/main/scala/com/jhruzik/wahlrechtscraper/Main.scala
+++ src/main/scala/com/jhruzik/wahlrechtscraper/Main.scala
@@ -2,11 +2,19 @@ package com.jhruzik.wahlrechtscraper
 
 object Main extends App {
 
-  if (args.length == 0 || args(0).toLowerCase() == "bund") {
-    println(Sonntagsfrage.bund())
-  }
-  else if (args(0).toLowerCase() == "state") {
-    println(Sonntagsfrage.states())
-  }
+  // Match Arguments
+  if (args.length > 0) {
+
+    // Sanity Check on User Input
+    val args_allowed = Set("bund", "states")
+    val error_msg = "Please choose one argument. Either bund or states"
+    val arg = args(0).toLowerCase()
+    if (args.length > 1) throw new IllegalArgumentException(error_msg)
+    if (!args_allowed.contains(arg)) throw new IllegalArgumentException(error_msg)
+
+    // Return Value
+    if (arg == "bund") println(Sonntagsfrage.bund())
+    if (arg == "states") println(Sonntagsfrage.states())
+  } else println(Sonntagsfrage.bund())
 
 }
